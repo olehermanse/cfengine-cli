951c3a64cdd7b8cdf388fce21e73f54cc98adb3a
Ole Herman Schumacher Elgesem <ole@northern.tech>
2026-05-01 04:08:50 +0200

    lint.py: Moved block type linting to separate function
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- src/cfengine_cli/lint.py
+++ src/cfengine_cli/lint.py
@@ -571,6 +571,17 @@ def _discover(policy_file: PolicyFile, state: State) -> int:
     return 0
 
 
+def _lint_block_type(node: Node, state: State, location: str, syntax_data: SyntaxData):
+    if (
+        node.type == "bundle_block_type"
+        and _text(node) not in syntax_data.BUILTIN_BUNDLE_TYPES
+    ):
+        raise ValidationError(
+            f"Error: Bundle type must be one of ({', '.join(syntax_data.BUILTIN_BUNDLE_TYPES)}), not '{_text(node)}' {location}",
+            node,
+        )
+
+
 def _lint_block_name(node: Node, state: State, location: str, syntax_data: SyntaxData):
     assert node.type in ("bundle_block_name", "body_block_name", "promise_block_name")
 
@@ -868,14 +879,9 @@ def _lint_node(
             raise ValidationError(
                 f"Error: Undefined promise type '{promise_type}' {location}", node
             )
-    if (
-        node.type == "bundle_block_type"
-        and _text(node) not in syntax_data.BUILTIN_BUNDLE_TYPES
-    ):
-        raise ValidationError(
-            f"Error: Bundle type must be one of ({', '.join(syntax_data.BUILTIN_BUNDLE_TYPES)}), not '{_text(node)}' {location}",
-            node,
-        )
+
+    if node.type in ("bundle_block_type", "body_block_type", "promise_block_type"):
+        _lint_block_type(node, state, location, syntax_data)
     if node.type in ("bundle_block_name", "body_block_name", "promise_block_name"):
         _lint_block_name(node, state, location, syntax_data)
     if node.type == "promise":
